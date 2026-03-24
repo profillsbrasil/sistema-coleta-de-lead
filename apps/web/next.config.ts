b674d96954757f37de04a4c4678aaa7a4ec5fe23
Othavio Quiliao <othavioaqa@gmail.com>
2026-03-24 12:58:56 -0300

    feat: primeiro comit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@dashboard-leads-profills/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
