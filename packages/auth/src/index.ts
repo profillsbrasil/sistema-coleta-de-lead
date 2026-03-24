aa2cbfa71904234797a13f4f0871143b8f6bca99
Othavio Quiliao <othavioaqa@gmail.com>
2026-03-24 12:58:56 -0300

    feat: primeiro comit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,21 @@
+import { db } from "@dashboard-leads-profills/db";
+import * as schema from "@dashboard-leads-profills/db/schema/auth";
+import { env } from "@dashboard-leads-profills/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  plugins: [nextCookies()],
+});
