35f0d08d0c3f2c1ecb8dcd430e43aa8c82832bc2
Othavio Quiliao <othavioaqa@gmail.com>
2026-04-17 22:51:44 -0300

    feat(auth): handler Next.js /api/auth/[...all] + client React

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,1 +1,9 @@
-export const clientPlaceholder = "better-auth-client-tbd";
+import { createAuthClient } from "better-auth/react";
+import { adminClient } from "better-auth/client/plugins";
+
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  plugins: [adminClient()],
+});
+
+export const { signIn, signUp, signOut, useSession } = authClient;
